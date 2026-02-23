fb0cc63408d84c40fa3fcf6c02960b186be07189
soccersanskar <soccersanskar@gmail.com>
2026-02-22 21:49:05 -0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js'
+import path from 'path' // We need this to handle the @ symbol
 
 export default defineConfig({
   plugins: [
     react(),
     cssInjectedByJsPlugin()
   ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"), // This fixes the error!
+    },
+  },
   build: {
-    target: 'esnext',
     rollupOptions: {
       output: {
         manualChunks: undefined,
